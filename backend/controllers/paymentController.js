a4d957bd3336e1451937206f77e863cd472e8af1
victor akubo <victorugbede89@gmail.com>
2026-03-01 14:07:57 +0100

    new commit

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,7 +1,6 @@
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-
 export const PaymentCheckout =  async (req, res) => {
   try {
     const { cartItem } = req.body;
